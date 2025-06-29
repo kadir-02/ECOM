5561152697e922204beae9faae00034cd1b839c7
kadir-02 <kadirjanaka02@gmail.com>
2025-06-29 14:18:30 +0530

    "migrations-deleted"

--- prisma/migrations/20250629081138_email_verifications/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `veiryOTP` on the `EmailVerify` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "EmailVerify" DROP COLUMN "veiryOTP",
-ADD COLUMN     "verifyOTP" TEXT;
