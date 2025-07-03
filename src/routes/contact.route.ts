63a70eb9232b86aa17e9eede8076d7fc1f0039e2
kadir-02 <kadirjanaka02@gmail.com>
2025-07-03 19:07:51 +0530

    "cantact"

--- src/routes/contact.route.ts
+++ src/routes/contact.route.ts
@@ -10,8 +10,8 @@ import { authenticate } from '../middlewares/authenticate';
 import { authorizeAdmin } from '../middlewares/authorizaAdmin';
 
 const router = express.Router();
-router.use(authenticate)
 router.post('/contact_form', createContactRequest);
+router.use(authenticate)
 router.get('/contact_form',authorizeAdmin, getContactRequests);
 router.get('/contacts/:id',authorizeAdmin, getContactRequestById);
 router.patch('/contact_form/:id',authorizeAdmin, markContactAsHandled);
