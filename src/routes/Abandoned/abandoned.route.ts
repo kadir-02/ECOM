0d6505dbedcedd0bf8ef0528177ce7c6ecc617c0
kadir-02 <kadirjanaka02@gmail.com>
2025-07-18 11:17:47 +0530

    "test-abandoned"

--- src/routes/Abandoned/abandoned.route.ts
+++ src/routes/Abandoned/abandoned.route.ts
@@ -5,11 +5,13 @@ import {
   getUsersSpecificAbandonedItems
 } from '../../controllers/Abandoned/abandoned.controller';
 import { authenticate } from '../../middlewares/authenticate';
+import { testAbandonedcontroller } from '../../controllers/Abandoned/testAbandonedcontroller';
 
 const router = Router();
 
 router.post('/apply-discount', authenticate, applyAbandonedCartDiscount);
 router.get('/get-discount', authenticate, getAbandonedCartDiscount); 
 router.get('/items', authenticate, getUsersSpecificAbandonedItems); 
+router.get('/get', authenticate, testAbandonedcontroller); 
 
 export default router;
