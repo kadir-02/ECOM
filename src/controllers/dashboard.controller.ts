d12ec28a57bef02cef331b3838f9ffd39a45fb00
ganesh19-afk <ganesh.magar@consociatesolutions.com>
2025-07-23 09:48:06 +0530

    "date fixed"

--- src/controllers/dashboard.controller.ts
+++ src/controllers/dashboard.controller.ts
@@ -184,7 +184,7 @@ const end = new Date(endIST.getTime() - IST_OFFSET);
         where: {
           createdAt: {
             gte: start,
-            lt: new Date(end.getTime() + 24 * 60 * 60 * 1000), // include entire end date
+            lt: end, // include entire end date
           },
           // no userId filter here to get all users' orders
         },
